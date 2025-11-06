1d4e73ec5ffb4be47f634f4243ef263f6a012949
Mehdi Raza <157884983+mehdi-raza99@users.noreply.github.com>
2025-11-06 18:23:38 +0500

    Time: 0 ms (100%), Space: 97 MB (51.29%) - LeetHub

--- 2866-longest-even-odd-subarray-with-threshold/2866-longest-even-odd-subarray-with-threshold.cpp
+++ 2866-longest-even-odd-subarray-with-threshold/2866-longest-even-odd-subarray-with-threshold.cpp
@@ -5,9 +5,8 @@ public:
         for(int i=0;i<size;i++){
             if(nums[i] % 2 == 0 && nums[i] <= threshold){
                 start=i;
-                while((i < size-1) && (nums[i] % 2 != nums[i + 1] % 2) && (nums[i] <= threshold && nums[i+1] <= threshold)){
+                while((i < size-1) && (nums[i] % 2 != nums[i + 1] % 2) && (nums[i+1] <= threshold))
                     i++;
-                }
                 end=i;
                 result= (end-start)+1 > result ?  (end-start)+1 : result;
             }
