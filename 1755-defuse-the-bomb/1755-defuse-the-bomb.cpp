7e1a0a5ec9978f64af9bd892ff424c13305ef52c
Mehdi Raza <157884983+mehdi-raza99@users.noreply.github.com>
2025-11-06 06:12:32 +0500

    Time: 0 ms (100%), Space: 11.6 MB (80.72%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        vector<int> result;
+        int size=code.size();
+        if(k == 0){
+            vector<int> result(size,0);
+            return result;
+        }
+        else if(k > 0){
+            int sum=0, start=1,end=start+k-1;
+            for(int i=start;i<=end;i++) sum+=code[i];
+            result.push_back(sum);
+            while(result.size() != code.size()){
+                sum-=code[start % size];
+                start++,end++;
+                sum+=code[end % size];
+                result.push_back(sum);
+            }
+        }
+            else{
+                int sum=0, start=size-abs(k),end=start+abs(k)-1;
+                for(int i=start;i<=end;i++) sum+=code[i];
+                result.push_back(sum);
+                while(result.size() != code.size()){
+                    sum-=code[start % size];
+                    start++,end++;
+                    sum+=code[end % size];
+                    result.push_back(sum);
+            }
+        }
+        return result;
+    }
+};
