a5237e2ded12fbed22605fcb1d2140e48dfd2656
Mehdi Raza <157884983+mehdi-raza99@users.noreply.github.com>
2025-11-18 07:03:21 +0500

    Time: 0 ms (100%), Space: 14.2 MB (65.78%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -2,12 +2,20 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
-        set<int> mySet;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
         set<int> resultSet;
-        for(int number: nums1) mySet.insert(number);
-
-        for(int number: nums2){
-            if(mySet.find(number) != mySet.end()) resultSet.insert(number);
+        int n1=nums1.size(),n2=nums2.size(),index1=0,index2=0;
+        while(index1 < n1 && index2 < n2){
+            if(nums1[index1] == nums2[index2]){
+                resultSet.insert(nums1[index1]);
+                index1++,index2++;
+                continue;
+            }
+            if(nums1[index1] < nums2[index2])
+                index1++;
+            else
+                index2++;
         }
 
         for(const auto& element : resultSet) {
