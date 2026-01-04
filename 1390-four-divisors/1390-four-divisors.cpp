ada550efeef72df99dc586088143a971a486f52a
Mehdi Raza <157884983+mehdi-raza99@users.noreply.github.com>
2026-01-04 11:59:04 +0500

    Time: 12 ms (70.94%), Space: 16.2 MB (71.43%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<int> getDivisors(int num){
-        vector<int> result;
-        if(num > 1){
-            result.push_back(1);
-            result.push_back(num);
-        }
-        else result.push_back(1);
-
-        for(int i=2;i<=sqrt(num);i++){
-            if(num%i == 0){
-                result.push_back(i);
-                if(i != (num/i)) result.push_back(num/i);
-            }
-        }
-        return result;
-    }
-
-    int getSum(vector<int> divisors){
-        int sum=0;
-        for(int i: divisors) sum+=i;
-        return sum;
-    }
     int sumFourDivisors(vector<int>& nums) {
-        int result=0,n=nums.size();
-        for(int i=0;i<n;i++){
-            vector<int> divisorList=getDivisors(nums[i]);
-            if(divisorList.size() == 4){
-                result+=getSum(divisorList);
+        int result = 0;
+
+        for (int num : nums) {
+            int cnt = 0, sum = 0;
+
+            for (int i = 1; i * i <= num; i++) {
+                if (num % i == 0) {
+                    int d1 = i;
+                    int d2 = num / i;
+
+                    cnt++;
+                    sum += d1;
+
+                    if (d1 != d2) {
+                        cnt++;
+                        sum += d2;
+                    }
+
+                    if (cnt > 4) break;
+                }
             }
+
+            if (cnt == 4) result += sum;
         }
-    return result;
+
+        return result;
     }
 };
