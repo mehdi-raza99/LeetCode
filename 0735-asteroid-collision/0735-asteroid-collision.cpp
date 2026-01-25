0ad732ce47aec935e5da8bb20e25e7ec0622728b
Mehdi Raza <157884983+mehdi-raza99@users.noreply.github.com>
2026-01-25 20:23:55 +0500

    Time: 7 ms (5.88%), Space: 22.1 MB (38.89%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -6,27 +6,19 @@ public:
         for(int i=0;i<asteroids.size();i++){
             if(asteroids[i] > 0){
                 st.push(asteroids[i]);
-                continue;
             }
-            int absoluteValue=abs(asteroids[i]);
-            while(!st.empty() && st.top() > 0 && st.top() < absoluteValue)
-                st.pop();
-            if(!st.empty() && st.top() == absoluteValue){ //Case when equal size asteroiod meet
-                st.pop();
-                continue;
+            else{
+                int absoluteValue=abs(asteroids[i]);
+                while(!st.empty() && st.top() > 0 && st.top() < absoluteValue)
+                    st.pop();
+                if(!st.empty() && st.top() == absoluteValue){ //Case when equal size asteroiod meet
+                    st.pop();
+                    continue;
+                }
+                if(st.empty() || st.top() < 0)
+                    st.push(asteroids[i]);
             }
-            if(st.empty() || st.top() < 0)
-                st.push(asteroids[i]);
         }
-        //   stack<int> st2; //Using another stack to reverse element
-        //   while(!st.empty()){
-        //     st2.push(st.top());
-        //     st.pop();
-        //   }
-        //   while(!st2.empty()){
-        //     result.push_back(st2.top());
-        //     st2.pop();
-        //   }
         while(!st.empty()){
             result.insert(result.begin(),st.top());
             st.pop();
